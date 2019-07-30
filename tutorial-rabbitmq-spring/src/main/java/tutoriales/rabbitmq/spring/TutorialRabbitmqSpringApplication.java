1e803ad00a74a993c2c027cd2cbc8a213fdbd7f8
fseeber <seeberfederico@gmail.com>
2019-07-30 16:55:56 -0300

    Add files via upload

---
+++ tutorial-rabbitmq-spring/src/main/java/tutoriales/rabbitmq/spring/TutorialRabbitmqSpringApplication.java
@@ -0,0 +1,30 @@
+package tutoriales.rabbitmq.spring;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialRabbitmqSpringApplication implements CommandLineRunner {
+
+    private static final String MESSAGE = "Hello world!";
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialRabbitmqSpringApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws InterruptedException {
+    	System.out.println("[Application] Enviando el mensaje 1 \"" + MESSAGE + "\"...");
+        rabbitTemplate.convertAndSend(RabbitMqConfig.EXCHANGE_NAME, RabbitMqConfig.ROUTING_KEY, MESSAGE);
+        System.out.println("[Application] Enviando el mensaje 2 \"" + MESSAGE + "\"...");
+        rabbitTemplate.convertAndSend(RabbitMqConfig.EXCHANGE_NAME, RabbitMqConfig.ROUTING_KEY, MESSAGE);
+        System.out.println("[Application] Enviando el mensaje 3 \"" + MESSAGE + "\"...");
+        rabbitTemplate.convertAndSend(RabbitMqConfig.EXCHANGE_NAME, RabbitMqConfig.ROUTING_KEY, MESSAGE);
+    }
+}
